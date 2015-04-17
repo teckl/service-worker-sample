8c67f9cd0d1e8bdd593d8b4b02cb94629e03be2b
Shigeki Sugai <teckl1979@gmail.com>
2015-04-17 11:40:08 +0900

    fix js

--- push/service-worker.js
+++ push/service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-importScripts('/push/scripts/indexdbwrapper.js');
+importScripts('../push/scripts/indexdbwrapper.js');
 
 var YAHOO_WEATHER_API_ENDPOINT = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22london%2C%20uk%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
 var KEY_VALUE_STORE_NAME = 'key-value-store';
